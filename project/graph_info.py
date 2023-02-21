50e0147bd826735b94a2f71dd4e94721eead218a
RazerFord <desk10567@gmail.com>
2023-02-21 12:09:21 +0300

    Упрости название аргументов

--- project/graph_info.py
+++ project/graph_info.py
@@ -5,20 +5,18 @@ import networkx
 def get_info(graph_name: str):
     graph_path = cfpq_data.download(graph_name)
     graph = cfpq_data.graph_from_csv(graph_path)
-    count_nodes, count_edges = graph.number_of_nodes(), graph.number_of_edges()
+    num_node, num_edge = graph.number_of_nodes(), graph.number_of_edges()
     labels = set()
     for v, u in graph.edges():
         if "label" in graph.get_edge_data(v, u, 0, {}):
             labels.add(graph.get_edge_data(v, u, 0, {})["label"])
 
-    return count_nodes, count_edges, labels
+    return num_node, num_edge, labels
 
 
-def create_graph_two_cycles(
-    count_node_first, count_node_second, labels, file_name="output.dot"
-):
+def create_graph_two_cycles(num_node_first, num_node_second, labels, path="output.dot"):
     graph = cfpq_data.labeled_two_cycles_graph(
-        count_node_first, count_node_second, labels=labels
+        num_node_first, num_node_second, labels=labels
     )
     graph = networkx.drawing.nx_pydot.to_pydot(graph)
-    graph.write_raw(file_name)
+    graph.write_raw(path)
