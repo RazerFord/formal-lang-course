c598e9576e8fba100578bff2f0c50ac46758d418
RazerFord <desk10567@gmail.com>
2023-02-26 18:59:26 +0300

    Исправил названия функций и добавил namedtuple

--- project/graph_info.py
+++ project/graph_info.py
@@ -1,22 +1,34 @@
 import cfpq_data
 import networkx
+from collections import namedtuple
+
+GraphInfo = namedtuple("GraphInfo", "number_of_nodes number_of_edges labels")
 
 
-def get_info(graph_name):
+def get_graph_info_by_name(graph_name):
     graph_path = cfpq_data.download(graph_name)
     graph = cfpq_data.graph_from_csv(graph_path)
-    num_node, num_edge = graph.number_of_nodes(), graph.number_of_edges()
+
+    labels = parse_labels(graph)
+    graph_info = GraphInfo(graph.number_of_nodes(), graph.number_of_edges(), labels)
+
+    return graph_info
+
+
+def parse_labels(graph):
     labels = set()
-    for v, u in graph.edges():
-        if "label" in graph.get_edge_data(v, u, 0, {}):
-            labels.add(graph.get_edge_data(v, u, 0, {})["label"])
+    for _, _, ddict in graph.edges(data=True):
+        label = ddict.get("label", None)
+        if label is not None:
+            labels.add(label)
+    return labels
 
-    return num_node, num_edge, labels
 
-
-def create_graph_two_cycles(num_node_first, num_node_second, labels, path="output.dot"):
+def create_and_save_graph_two_cycles(
+    num_node_fst, num_node_snd, labels, path="out.dot"
+):
     graph = cfpq_data.labeled_two_cycles_graph(
-        num_node_first, num_node_second, labels=labels
+        num_node_fst, num_node_snd, labels=labels
     )
     graph = networkx.drawing.nx_pydot.to_pydot(graph)
     graph.write_raw(path)
