f07249af40855fb545d1f7b0296e1c6b54285f84
RazerFord <desk10567@gmail.com>
2023-03-10 05:37:43 +0300

    Вынес основную логику в класс

--- project/intersection_finite_automata.py
+++ project/intersection_finite_automata.py
@@ -55,91 +55,165 @@ class Mapping:
         return self._invert_mapp
 
 
+class IntersectionFiniteAutomata:
+    """
+    Class for finding the intersection of two finite automata
+
+    Parameters
+    ----------
+    fst_automata : DeterministicFiniteAutomaton
+        First deterministic finite automata
+    snd_automata : DeterministicFiniteAutomaton
+        Second deterministic finite automata
+    """
+
+    def __init__(
+        self,
+        fst_automata: DeterministicFiniteAutomaton,
+        snd_automata: DeterministicFiniteAutomaton,
+    ):
+        self.enfa = EpsilonNFA()
+        self._init_states(fst_automata, snd_automata)
+        self.symbols = set(list(fst_automata.symbols) + list(snd_automata.symbols))
+        self._fst_decompos, self._fst_mp = self._get_boolean_decomposition_and_map(
+            fst_automata
+        )
+        self._snd_decompos, self._snd_mp = self._get_boolean_decomposition_and_map(
+            snd_automata
+        )
+        self._mtrxes = self._calculate_kron()
+        self._add_transitions(len(snd_automata.states))
+
+    def _init_states(
+        self,
+        fst_automata: DeterministicFiniteAutomaton,
+        snd_automata: DeterministicFiniteAutomaton,
+    ):
+        start_states = IntersectionFiniteAutomata.get_intersection_states(
+            fst_automata.start_states, snd_automata.start_states
+        )
+        for start_state in start_states:
+            for state in start_state:
+                self.enfa.add_start_state(state)
+
+        final_states = IntersectionFiniteAutomata.get_intersection_states(
+            fst_automata.final_states, snd_automata.final_states
+        )
+        for final_state in final_states:
+            for state in final_state:
+                self.enfa.add_final_state(state)
+
+    def _get_boolean_decomposition_and_map(
+        self, automata: DeterministicFiniteAutomaton
+    ) -> tuple[dict[str, sp.lil_matrix], Mapping]:
+        decomposition = {}
+        number_states = len(automata.states)
+
+        for label in self.symbols:
+            decomposition[label] = sp.lil_matrix(
+                (number_states, number_states), dtype=int
+            )
+
+        mapp = Mapping(automata.states)
+        mp = mapp.get_map()
+
+        for u, l, v in automata:
+            decomposition[l.value][mp[u.value], mp[v.value]] = 1
+
+        return decomposition, mapp
+
+    def _calculate_kron(self):
+        mtrxes = {}
+        for symbol in self.symbols:
+            mtrxes[symbol] = sp.kron(
+                self._fst_decompos[symbol], self._snd_decompos[symbol]
+            )
+        return mtrxes
+
+    def _add_transitions(self, num: int):
+        fst_imp = self._fst_mp.get_imap()
+        snd_imp = self._snd_mp.get_imap()
+        for key, mtrx in self._mtrxes.items():
+            for i, row in enumerate(mtrx.toarray()):
+                for j, val in enumerate(row):
+                    if val == True:
+                        fr = IntersectionFiniteAutomata.combine_state_pair(
+                            State(fst_imp[i // num]), State(snd_imp[i % num])
+                        )
+                        to = IntersectionFiniteAutomata.combine_state_pair(
+                            State(fst_imp[j // num]), State(snd_imp[j % num])
+                        )
+                        self.enfa.add_transition(fr, key, to)
+
+    @staticmethod
+    def combine_state_pair(fst_state: State, snd_state: State) -> State:
+        """
+        Parameters
+        ----------
+        fst_state : State
+            State of the first graph
+        snd_state : State
+            State of the second graph
+
+        Returns
+        ----------
+        State
+            Returns the union of two states into one
+        """
+        return State(str(fst_state.value) + "; " + str(snd_state.value))
+
+    @staticmethod
+    def get_intersection_states(
+        fst_states: list[State], snd_states: list[State]
+    ) -> list[list[State]]:
+        """
+        Parameters
+        ----------
+        fst_states : list[State]
+            States of the first graph
+        snd_states : list[State]
+            States of the second graph
+
+        Returns
+        ----------
+        list[list[State]]
+            Returns a combination of all graph states
+        """
+        mtrx = []
+        for i, fst in enumerate(fst_states):
+            mtrx.append([])
+            for snd in snd_states:
+                mtrx[i].append(IntersectionFiniteAutomata.combine_state_pair(fst, snd))
+        return mtrx
+
+    def get_enfa(self) -> EpsilonNFA:
+        """
+        Returns
+        ----------
+        EpsilonNFA
+            Return NFA
+        """
+        return self.enfa
+
+
 def get_intersection_two_finite_automata(
     fst_automata: DeterministicFiniteAutomaton,
     snd_automata: DeterministicFiniteAutomaton,
 ):
-    enfa = EpsilonNFA()
+    """
+    Parameters
+    ----------
+    fst_automata : DeterministicFiniteAutomaton
+        First deterministic finite automata
+    snd_automata : DeterministicFiniteAutomaton
+        Second deterministic finite automata
 
-    start_states = get_intersection_states(
-        fst_automata.start_states, snd_automata.start_states
-    )
-    for start_state in start_states:
-        for state in start_state:
-            enfa.add_start_state(state)
-
-    final_states = get_intersection_states(
-        fst_automata.final_states, snd_automata.final_states
-    )
-    for final_state in final_states:
-        for state in final_state:
-            enfa.add_final_state(state)
-
-    symbols = set(list(fst_automata.symbols) + list(snd_automata.symbols))
-
-    fst_decomposition, fst_map = get_boolean_decomposition_and_map(
-        fst_automata, symbols
-    )
-    snd_decomposition, snd_map = get_boolean_decomposition_and_map(
-        snd_automata, symbols
-    )
-
-    mtrxes = {}
-    for symbol in symbols:
-        mtrxes[symbol] = sp.kron(fst_decomposition[symbol], snd_decomposition[symbol])
-
-    num = len(snd_automata.states)
-    fst_imp = fst_map.get_imap()
-    snd_imp = snd_map.get_imap()
-    for key, mtrx in mtrxes.items():
-        for i, row in enumerate(mtrx.toarray()):
-            for j, val in enumerate(row):
-                if val == True:
-                    fr = combine_state_pair(
-                        State(fst_imp[i // num]), State(snd_imp[i % num])
-                    )
-                    to = combine_state_pair(
-                        State(fst_imp[j // num]), State(snd_imp[j % num])
-                    )
-                    enfa.add_transition(fr, key, to)
-    return enfa
-
-
-def combine_state_pair(fst_state: State, snd_state: State) -> State:
-    return State(str(fst_state.value) + "; " + str(snd_state.value))
-
-
-def get_intersection_states(
-    fst_states: list[State], snd_states: list[State]
-) -> list[list[State]]:
-    mtrx = []
-    for i, fst in enumerate(fst_states):
-        mtrx.append([])
-        for snd in snd_states:
-            mtrx[i].append(combine_state_pair(fst, snd))
-    return mtrx
-
-
-def states_to_list_int(states: list[int]) -> list[int]:
-    return [x.value for x in list(states)]
-
-
-def get_boolean_decomposition_and_map(
-    automata: DeterministicFiniteAutomaton, symbols: set
-) -> tuple[dict[str, sp.lil_matrix], Mapping]:
-    decomposition = {}
-    number_states = len(automata.states)
-
-    for label in symbols:
-        decomposition[label] = sp.lil_matrix((number_states, number_states), dtype=int)
-
-    mapp = Mapping(automata.states)
-    mp = mapp.get_map()
-
-    for u, l, v in automata:
-        decomposition[l.value][mp[u.value], mp[v.value]] = 1
-
-    return decomposition, mapp
+    Returns
+    ----------
+        Returns the intersection of two finite automata
+    """
+    intersection = IntersectionFiniteAutomata(fst_automata, snd_automata)
+    return intersection.get_enfa()
 
 
 def regular_query_to_graph(
@@ -148,6 +222,24 @@ def regular_query_to_graph(
     start_nodes: list = None,
     final_nodes: list = None,
 ) -> list[tuple[State, State]]:
+    """
+    Parameters
+    ----------
+    regex_expr : str
+        Regular expression written as a string
+    graph : nx.MultiDiGraph
+        The graph from which nfa is built
+    start_nodes : list[State]
+        List of start nodes
+    final_nodes : list[State]
+        List of final nodes
+        
+    Returns
+    ----------
+    list[tuple[State, State]]
+        Returns a list of pairs such that the first 
+        element of each pair is reachable from the second
+    """
     dfa_from_reg = fa.create_deterministic_automaton_from_regex(regex_expr)
     dfa_from_graph = fa.create_non_deterministic_automaton_from_graph(
         graph, start_nodes, final_nodes
