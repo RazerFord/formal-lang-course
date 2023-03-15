75b867a5b4c49171a25ddea4a0b9c189a4e02cf9
RazerFord <desk10567@gmail.com>
2023-03-16 02:00:43 +0300

    Реализовал модифицированный алгоритм

--- project/reachable_regular_constraints.py
+++ project/reachable_regular_constraints.py
@@ -12,6 +12,10 @@ def get_reachable_vertices(
     automata: fa.DeterministicFiniteAutomaton,
     start_nodes: list[int],
 ):
+    if len(start_nodes) > len(graph.nodes):
+        raise ValueError(
+            "The number of starting vertices is greater than the vertices in the graph"
+        )
     labels_gr = parse_labels(graph)
     labels_fa = automata.symbols
 
@@ -20,42 +24,56 @@ def get_reachable_vertices(
     labels = labels_fa.intersection(labels_gr)
     l = list(labels)[0]
     matrices = combine_matrix(matrix_fa, matrix_gr, labels)
-    matrix_transition = init_matrix_transition(matrix_fa[l], matrix_gr[l])
-    offset, length = matrix_transition.shape
-    mp = mp_fa.get_map()
+    matrix_transition = init_matrix_transition(
+        matrix_fa[l].shape[0], matrix_gr[l].shape[1], len(start_nodes)
+    )
+    height, length = matrix_transition.shape
+    offset = height // len(start_nodes)
+    mp_fa_to_indx = mp_fa.get_map()
+    mp_gr_to_indx = mp_gr.get_map()
 
     for start in automata.start_states:
-        indx = mp[start]
-        for node in start_nodes:
-            matrix_transition[indx, node + offset] = 1
+        indx = mp_fa_to_indx[start]
+        for i, node in enumerate(start_nodes):
+            indx_node = mp_gr_to_indx[node]
+            matrix_transition[indx + i * offset, indx_node + offset] = 1
 
     count_non_zeros = -1
-    visibles = sp.lil_matrix(matrix_transition, dtype=bool)
-    start_matrix = sp.lil_matrix(matrix_transition, dtype=bool)
+    visibles = matrix_transition.copy()
+    start_matrix = matrix_transition.copy()
 
     while visibles.count_nonzero() != count_non_zeros:
         count_non_zeros = visibles.count_nonzero()
-        next_matrix = sp.eye(offset, length, dtype=bool, format="lil")
+        next_matrix = init_matrix_transition(
+            matrix_fa[l].shape[0], matrix_gr[l].shape[1], len(start_nodes)
+        )
 
         for matrix in matrices.values():
             new_matrix = matrix_transition @ matrix
-            for i in range(offset):
-                for j in range(offset):
-                    if new_matrix[i, j]:
-                        next_matrix[j, offset:] += new_matrix[i, offset:]
+            for k in range(len(start_nodes)):
+                for i in range(offset):
+                    for j in range(offset):
+                        if new_matrix[i + offset * k, j]:
+                            next_matrix[j + offset * k, offset:] += new_matrix[
+                                i + offset * k, offset:
+                            ]
         matrix_transition = next_matrix
         visibles += next_matrix
 
     matrix_result = np.logical_xor(visibles.toarray(), start_matrix.toarray())
 
     mp_to_node = mp_gr.get_imap()
+    res_u_v = set()
     res = set()
-    for final in automata.final_states:
-        indx = mp[final]
-        for i in range(offset, length, 1):
-            if matrix_result[indx, i]:
-                res.add(mp_to_node[i - offset])
-    return res
+    for k, v in enumerate(start_nodes):
+        for final in automata.final_states:
+            print(automata.final_states)
+            indx = mp_fa_to_indx[final]
+            for i in range(offset, length, 1):
+                if matrix_result[indx + k * offset, i]:
+                    res_u_v.add((v, mp_to_node[i - offset]))
+                    res.add(mp_to_node[i - offset])
+    return res_u_v, res
 
 
 def combine_matrix(
@@ -76,16 +94,16 @@ def combine_matrix(
 
 
 def init_matrix_transition(
-    matrix_fa: sp.lil_matrix, matrix_gr: sp.lil_matrix
+    number_rows_fa: int, number_colmn_gr: int, n: int
 ) -> dict[str, sp.lil_matrix]:
-    number_rows_fa, _ = matrix_fa.shape
-    _, number_colmn_gr = matrix_gr.shape
-    i_matrix = sp.lil_matrix(sp.eye(number_rows_fa))
-    matrix = sp.lil_matrix(
-        sp.hstack(
-            [i_matrix, sp.lil_matrix((number_rows_fa, number_colmn_gr), dtype=bool)]
+    template = sp.lil_matrix(
+        sp.eye(
+            number_rows_fa, number_rows_fa + number_colmn_gr, dtype=bool, format="lil"
         )
     )
+    matrix = template
+    for _ in range(n - 1):
+        matrix = sp.lil_matrix(sp.vstack([matrix, template]), dtype=bool)
     return matrix
 
 
@@ -130,10 +148,11 @@ def init_mapping_and_matrix(
 
 def init_graph():
     gr = nx.MultiDiGraph()
-    gr.add_edge(0, 6, label="b")
-    gr.add_edge(6, 0, label="b")
+    gr.add_edge(0, 3, label="b")
+    gr.add_edge(3, 0, label="b")
     gr.add_edge(0, 1, label="a")
     gr.add_edge(1, 2, label="b")
+    gr.add_edge(3, 2, label="b")
     gr.add_edge(2, 0, label="a")
     return gr
 
@@ -151,4 +170,4 @@ def init_regex():
 gr = init_graph()
 rx = init_regex()
 
-print(get_reachable_vertices(gr, rx, [0,  1]))
+print(get_reachable_vertices(gr, rx, [1]))
