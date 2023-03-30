564ea11832c98019927023229e52a4b632f9c8f9
RazerFord <desk10567@gmail.com>
2023-03-30 15:04:08 +0300

    Сделал вторую функцию

--- project/context_free_grammar.py
+++ project/context_free_grammar.py
@@ -1,7 +1,8 @@
 from pyformlang import cfg
+from pathlib import Path
 
 
-def cfg_to_wcnf(cfg_gr: cfg.CFG):
+def cfg_to_wcnf(cfg_gr: cfg.CFG) -> cfg.CFG:
     cfg_clear = (
         cfg_gr.remove_useless_symbols()
         .eliminate_unit_productions()
@@ -11,3 +12,20 @@ def cfg_to_wcnf(cfg_gr: cfg.CFG):
     prods = cfg_clear._get_productions_with_only_single_terminals()
     prods = cfg_clear._decompose_productions(prods)
     return cfg.CFG(start_symbol=cfg_clear.start_symbol, productions=set(prods))
+
+
+def read_cfg_from_file(file: str, start: cfg.Variable = None) -> cfg.CFG:
+    if not Path(file).is_file():
+        return None
+
+    with open(file) as cin:
+        text_cfg = "".join(cin.readlines())
+    if start is None:
+        cfg_res = cfg.CFG.from_text(text_cfg, cfg.Variable("S"))
+    else:
+        cfg_res = cfg.CFG.from_text(text_cfg, start)
+    return cfg_res
+
+
+def read_wcfg_from_file(file: str, start: cfg.Variable = None) -> cfg.CFG:
+    return cfg_to_wcnf(read_cfg_from_file(file, start))
