b61412c284326d562fd2fa497b1d34f2496ad0a3
RazerFord <desk10567@gmail.com>
2023-04-07 11:31:56 +0300

    Добавил преобразование к рекурсивным автоматам

--- project/ecfg.py
+++ project/ecfg.py
@@ -1,11 +1,10 @@
 from pyformlang.cfg import Production, Variable, Terminal, CFG, Epsilon
-from typing import AbstractSet, Iterable
+from pyformlang.rsa import Box, RecursiveAutomaton
 from pyformlang.cfg.utils import to_variable, to_terminal
 from pyformlang.regular_expression import Regex
-from pyformlang import rsa
 from context_free_grammar import read_cfg_from_file
-
-# from pyformlang.cfg import Production, Variable, Terminal, CFG, Epsilon
+from functools import reduce
+from typing import AbstractSet, Iterable
 
 
 class ECFG:
@@ -45,9 +44,8 @@ class ECFG:
         return self._variables
 
     def to_rsm(self):
-        print(list(self._productions))
-        boxes = [rsa.Box(e.head, e.body) for e in list(self._productions)]
-        return rsa(self._variables.self._start_symbol, boxes)
+        boxes = [Box(p.to_epsilon_nfa(), n) for n, p in self._productions.items()]
+        return RecursiveAutomaton(self._variables, self._start_symbol, boxes)
 
     @staticmethod
     def from_cfg(cfg: CFG):
@@ -56,31 +54,26 @@ class ECFG:
         start_symbol = cfg.start_symbol
         productions = {}
         for prod in cfg.productions:
-            if prod.head in productions:
-                raise OneRuleError()
-            body = prod.body
-            if len(body) != 0:
-                body = "".join(str(x.value) for x in prod.body)
-            else:
-                body = "$"
-            productions[prod.head] = body
-        prods = set()
+            productions.setdefault(prod.head, [])
+        for prod in cfg.productions:
+            p = prod.body
+            if not p:
+                p = [Variable("$")]
+            productions[prod.head].append(
+                reduce(Regex.concatenate, map(lambda x: Regex(x.value), p))
+            )
+        prods = {}
         for n, p in productions.items():
-            prods.add(Production(n, [Regex(p)]))
+            prods[n] = reduce(Regex.union, p)
         return ECFG(variables, terminals, start_symbol, prods)
 
     @staticmethod
-    def from_file(name_file: str):
-        return ECFG.from_cfg(read_cfg_from_file(name_file))
+    def from_file(name_file: str, start_sym: Variable = Variable("S")):
+        return ECFG.from_cfg(read_cfg_from_file(name_file, start_sym))
 
     @staticmethod
-    def from_str(text: str):
-        return ECFG.from_cfg(CFG.from_text(text))
-
-
-class OneRuleError(Exception):
-    def __init__(self) -> None:
-        super().__init__("Only one rule can exist for a non-terminal")
+    def from_str(text: str, start_sym: Variable = Variable("S")):
+        return ECFG.from_cfg(CFG.from_text(text, start_sym))
 
 
 S = Variable("S")
@@ -90,10 +83,10 @@ a = Terminal("a")
 b = Terminal("b")
 prods = []
 prods.append(Production(S, [A, S, A]))
-# prods.append(Production(S, [a, B]))
+prods.append(Production(S, [a, B]))
 prods.append(Production(A, [B]))
-# prods.append(Production(A, [S]))
-# prods.append(Production(B, [b]))
+prods.append(Production(A, [S]))
+prods.append(Production(B, [b]))
 prods.append(Production(B, [Epsilon()]))
 cfg = CFG(
     {S, A, B},
@@ -102,4 +95,4 @@ cfg = CFG(
     set(prods),
 )
 f = ECFG.from_cfg(cfg)
-f.to_rsm()
+rsm = f.to_rsm()
