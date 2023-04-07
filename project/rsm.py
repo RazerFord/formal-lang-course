978dbed128dd8c763fbbdf9ca56eff85db76a143
RazerFord <desk10567@gmail.com>
2023-04-08 02:47:57 +0300

    Добавил RSM

--- project/rsm.py
+++ project/rsm.py
@@ -1,10 +1,8 @@
-from pyformlang.rsa import Box
 from pyformlang.finite_automaton.finite_automaton import to_symbol, Symbol
-from typing import AbstractSet
 
 
 class RSM:
-    def __init__(self, start_label: Symbol, boxes: AbstractSet[Box]) -> None:
+    def __init__(self, start_label: Symbol, boxes) -> None:
         self._start_label = to_symbol(start_label) or Symbol("")
         self._boxes = boxes
 
