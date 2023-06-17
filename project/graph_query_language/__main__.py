bee9c70c70bae0cfb2274e0f6ef5e32323f79d10
RazerFord <desk10567@gmail.com>
2023-06-17 13:56:13 +0300

    added repl

--- project/graph_query_language/__main__.py
+++ project/graph_query_language/__main__.py
@@ -14,9 +14,13 @@ def read_file(file: Path)-> str:
 if __name__ == "__main__":
     if len(sys.argv) < 2:
         print_err("file name not passed")
-    file = Path(sys.argv[1])
-    if not file.is_file():
-        print_err("the given name is not exists")
-    if not file.name.endswith(".lng"):
-        print_err("invalid file format")
-    interpreter(read_file(file))
+    if len(sys.argv) == 2:
+        file = Path(sys.argv[1])
+        if not file.is_file():
+            print_err("the given name is not exists")
+        if not file.name.endswith(".lng"):
+            print_err("invalid file format")
+        interpreter(read_file(file))
+    if len(sys.argv) > 2:
+        prog = " ".join(sys.argv[1:]) + ";"
+        interpreter(prog)
