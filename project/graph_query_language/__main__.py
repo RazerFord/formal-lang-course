a8e58c9ef0c6e6c67e45f6f27163703b59703257
RazerFord <desk10567@gmail.com>
2023-06-17 11:35:31 +0300

    added print error message

--- project/graph_query_language/__main__.py
+++ project/graph_query_language/__main__.py
@@ -4,6 +4,8 @@ from exceptions import WrongNumberArguments
 from exceptions import InvalidArgument
 from interpreter import interpreter
 from pathlib import Path
+from utils import print_err
+
 
 def read_file(file: Path)-> str:
     text = file.open()
@@ -11,10 +13,10 @@ def read_file(file: Path)-> str:
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
-        raise WrongNumberArguments("file name not passed")
+        print_err("file name not passed")
     file = Path(sys.argv[1])
     if not file.is_file():
-        raise ScriptNotFound("the given name is not exists")
+        print_err("the given name is not exists")
     if not file.name.endswith(".lng"):
-        raise InvalidArgument("invalid file format")
+        print_err("invalid file format")
     interpreter(read_file(file))
--- project/graph_query_language/interpreter.py
+++ project/graph_query_language/interpreter.py
@@ -1,10 +1,16 @@
 from language_parser import build_parser
 from visitor import Visitor
+from utils import print_err
 
 def interpreter(text_prog: str) -> str:
     parser = build_parser(text_prog)
     tree = parser.program()
     visitor = Visitor()
 
-    visitor.visit(tree)
+    if parser.getNumberOfSyntaxErrors() != 0:
+        print_err("syntax errors found")
+    try:
+        visitor.visit(tree)
+    except Exception as e:
+        print_err(e.__str__())
     
